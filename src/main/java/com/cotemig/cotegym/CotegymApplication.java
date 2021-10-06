55f911f53d7168ad343ca919d359d27f6d361ceb
juan_ <juan_@DESKTOP-QLML9IM.local>
2021-10-06 20:07:56 -0300

    Criacao do projeto

---
+++ src/main/java/com/cotemig/cotegym/CotegymApplication.java
@@ -0,0 +1,13 @@
+package com.cotemig.cotegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotegymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotegymApplication.class, args);
+	}
+
+}
